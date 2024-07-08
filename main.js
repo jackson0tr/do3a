a260aab9d4792fc9c3c0bd5b35872b5f7c4def9d
jackson0tr <mahmoudmohamedm82@gmail.com>
2024-07-08 19:59:27 +0200

    4rth deploy

--- main.js
+++ main.js
@@ -8,46 +8,11 @@ const scrollRevealOption = {
 ScrollReveal().reveal(".container__left h1", {
   ...scrollRevealOption,
 });
-// ScrollReveal().reveal(".container__left .container__btn", {
-//   ...scrollRevealOption,
-//   delay: 500,
-// });
 
 ScrollReveal().reveal(".container__right h4", {
   ...scrollRevealOption,
   delay: 2000,
-});
-// ScrollReveal().reveal(".container__right h2", {
-//   ...scrollRevealOption,
-//   delay: 2500,
-// });
-// ScrollReveal().reveal(".container__right h3", {
-//   ...scrollRevealOption,
-//   delay: 3000,
-// });
-ScrollReveal().reveal(".container__right p", {
+});al().reveal(".container__right p", {
   ...scrollRevealOption,
-  delay: 2000,
+  delay: 2500,
 });
-
-// ScrollReveal().reveal(".container__right .tent-1", {
-//   duration: 1000,
-//   delay: 4000,
-// });
-// ScrollReveal().reveal(".container__right .tent-2", {
-//   duration: 1000,
-//   delay: 4500,
-// });
-
-// ScrollReveal().reveal(".location", {
-//   ...scrollRevealOption,
-//   origin: "left",
-//   delay: 5000,
-// });
-
-// ScrollReveal().reveal(".socials span", {
-//   ...scrollRevealOption,
-//   origin: "top",
-//   delay: 5500,
-//   interval: 500,
-// });
